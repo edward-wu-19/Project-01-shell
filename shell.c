ce2f8c29c15fea1245727a8da6491ff0d10e741a
LyfeMaster <64602623+LyfeMaster@users.noreply.github.com>
2021-11-18 11:00:54 -0500

    11/18 In class added ls and parse in shell

--- shell.c
+++ shell.c
@@ -1,11 +1,41 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+void bin_compilation();
+char **parse_args(char *line);
 
 void bin_compilation() {
     DIR *cwd = opendir("bin");
 
 }
 
-int main() {
+char **parse_args(char *line) {
+    // Figuring Out Argument Count by counting number of spaces
+    char *copy = strdup(line), *token;
+    int cnt = 1;
+    while (token = strsep(&line, " ")) {
+        cnt++;
+    }
+
+    // Creating Argument Array
+    char **A = calloc(cnt, sizeof(char *));
+
+    // Adding the pointers to the array
+    int i = 0;
+    while (token = strsep(&copy, " ")) {
+        A[i] = token;
+        i++;
+    }
+
+    // Returning Argument Array
+    return A;
+}
+
+int main(int argc, char *argv[]) {
+    char input[1024];
     // Making Sure All Binary Executables Are Created
     bin_compilation();
 
@@ -14,10 +44,17 @@ int main() {
         // Print Current Working Directory
 
         // Take In Commands
+        if (argc == 1){
+            fgets(input, 1024, stdin);
+            *strchr(input, '\n') = '\0';
+        }
 
         // Parse Commands
+        char **args = parse_args(input);
+        // printf("%s %s\n", args[0], args[1]);
 
         // Run Prgram From SRC
+        
     }
 
     // Ending Statement
--- src/ls.c
+++ src/ls.c
@@ -1,8 +1,57 @@
 #include "../shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <string.h>
 
-int main() {
+void return_info(DIR *pDir, char folderName[100]);
+
+int main(int argc, char *argv[]) {
     // Example Program Currently
 
     // Ending Statement
+
+    DIR *pDir = opendir(".");
+
+    if (pDir == 0){
+        printf("Error: %s\n\n", strerror(errno));
+        return 0;
+    }
+
+    return_info(pDir, ".");
+
     return 0;
+}
+
+void return_info(DIR *pDir, char folderName[100]){
+    struct dirent *dir;
+    dir = readdir(pDir);
+
+    printf("Statistics for Directory: %s\n\n", folderName);
+
+    unsigned long total_size = 0;
+
+    struct stat *buffer = malloc(sizeof(struct stat));
+
+    while (dir != 0){
+        if (dir->d_type != 4){
+            stat(dir->d_name, buffer);
+            printf("\t%s\t\tRegular file\t\t%ld bytes\n", dir->d_name, buffer->st_size);
+            total_size += buffer->st_size;
+        }
+        else{
+            printf("\t%s\t\tDirectory\n", dir->d_name);
+        }
+        dir = readdir(pDir);
+    }
+
+    printf("\n");
+    printf("Total Directory Size: %ld bytes", total_size);
+
+
+    closedir(pDir);
+    printf("\n\n");
 }
